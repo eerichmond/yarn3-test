850d8f1b5a06ac72992112583a8ec41583e9fdd9
Eli Richmond <eerichmond@ucdavis.edu>
2021-11-22 22:20:58 -0800

    test(local): undo duplicate jest.config.js hack

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,3 @@
-// HACK: duplicate the jest configs in the root of the monorepo for Wallaby.js
 module.exports = {
-  projects: [
-    {
-      displayName: 'client',
-      transform: { '^.+\\.js?$': 'esbuild-jest' },
-      testEnvironment: 'jsdom',
-      testMatch: ['<rootDir>/packages/client/**/*.test.js'],
-    },
-    {
-      displayName: 'server',
-      transform: { '^.+\\.js$': 'esbuild-jest' },
-      testMatch: ['<rootDir>/packages/server/**/*.test.js'],
-    },
-  ],
+  projects: ['packages/client', 'packages/server'],
 };
--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   transform: { '^.+\\.js$': 'esbuild-jest' },
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'], // This breaks the root level jest.config.js
 };
